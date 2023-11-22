1f2a8d47ba62fccdfb76f2b7c27400e4b9defab1
montana-rusa <emlcliffe@gmail.com>
2023-11-22 14:59:52 +0000

    wordLists added

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 var difficulty = "normal";
+wordList = [];
+
+startCoords = []
+endCoords = []
+selectedWords = []
 //global variables being declared
 
 function puzzlePress() {
@@ -26,6 +31,12 @@ function menuPress(b1, n1, n2, o1, on1, on2) {
 
 function changeDifficulty(newDifficulty) {
     difficulty = newDifficulty;
-    //document.getElementById("testText").innerHTML=difficulty; //this line was used to test that the value was changing
-} 
-//function changeDifficulty changes value of variable 'difficulty' when a new difficulty is clicked in settings
+    if (difficulty == "hard") {
+        wordList = ['auspice', 'aromatic', 'alleged', 'austere', 'ambient', 'apache', 'acumen', 'alias', 'brethren', 'behemoth', 'brink', 'berth', 'contempt', 'cervical', 'canopy', 'corpus', 'crust', 'defer', 'deacon', 'delusion', 'doctrine', 'ethereal', 'endemic', 'elitism', 'elope', 'ensue', 'effigy', 'eureka', 'enigma', 'foregone', 'ferment', 'fresco', 'grievous', 'galleon', 'gurney', 'gaunt', 'hooligan', 'holster', 'heretic', 'henna', 'impetus', 'incisor', 'ictus', 'irate', 'jetty', 'junkie', 'javelin', 'kamikaze', 'karate', 'kanji', 'kobold', 'lukewarm', 'lampoon', 'locale', 'lustre', 'macabre', 'matinee', 'mingle', 'macho', 'nutcase', 'neuroma', 'nubile', 'nacho', 'orifice', 'oration', 'oasis', 'opine', 'prophet', 'pastor', 'ponder', 'peril', 'quagmire', 'quiche', 'quark', 'reticent', 'relapse', 'roster', 'rabid', 'rapport', 'solitary', 'suppress', 'sierra', 'seismic', 'savory', 'trespass', 'turnout', 'thwart', 'trance', 'taboo', 'ukelele', 'unitard', 'umbra', 'usury', 'updraft', 'vampire', 'vermin', 'venial', 'wannabe', 'wanton', 'wallow', 'yakuza', 'yonder', 'yahoo', 'zonal', 'zephyr']
+    } if (difficulty == "normal") {
+        wordList = ['audience', 'amateur', 'accused', 'anomaly', 'absent', 'asylum', 'apathy', 'adept', 'anthem', 'beverage', 'blanket', 'behalf', 'blaze', 'coronary', 'candid', 'conduct', 'consent', 'census', 'drone', 'distress', 'dignity', 'dessert', 'elevator', 'eruption', 'epilogue', 'erosion', 'eclipse', 'eminent', 'erotic', 'equine', 'fugitive', 'fantasy', 'festive', 'fiber', 'genocide', 'glory', 'gallows', 'grudge', 'hysteria', 'hologram', 'hormone', 'hostage', 'inferior', 'indulge', 'incest', 'impair', 'idiom', 'jugular', 'justice', 'jungle', 'kilobyte', 'kneecap', 'kidney', 'karma', 'leisure', 'lunatic', 'launch', 'lesion', 'montage', 'modest', 'manor', 'magma', 'negative', 'neglect', 'nebula', 'ninja', 'outcast', 'ornate', 'organ', 'optimum', 'pregnant', 'preacher', 'plasma', 'quartet', 'quirk', 'quaint', 'release', 'reunion', 'riddle', 'retail', 'scarce', 'surgery', 'secular', 'surplus', 'shallow', 'scrap', 'throttle', 'tragic', 'triumph', 'thermal', 'thunder', 'ultimate', 'umpire', 'union', 'venture', 'valiant', 'vocal', 'welfare', 'whisper', 'wretch', 'yarrow', 'yacht', 'yeast', 'zealous', 'zodiac']
+    } if (difficulty == "easy") {
+        wordList = ['annoying', 'ability', 'author', 'artist', 'airport', 'algebra', 'arcade', 'angel', 'bright', 'blizzard', 'biology', 'birth', 'control', 'castle', 'crack', 'common', 'death', 'defeat', 'daughter', 'drunk', 'equality', 'eternal', 'earth', 'empire', 'effect', 'emotion', 'eyeball', 'ending', 'faith', 'failure', 'factory', 'family', 'general', 'gentle', 'global', 'garden', 'husband', 'hidden', 'hunger', 'heart', 'interest', 'infinite', 'island', 'insult', 'jigsaw', 'jacket', 'judge', 'juice', 'kitchen', 'kingdom', 'knight', 'knife', 'library', 'local', 'lethal', 'lawyer', 'marriage', 'morning', 'mirror', 'money', 'nickname', 'natural', 'nearby', 'nurse', 'original', 'option', 'office', 'owner', 'practice', 'paint', 'pasta', 'plain', 'quality', 'quick', 'quiet', 'quest', 'research', 'recover', 'ready', 'rough', 'solution', 'success', 'speech', 'storm', 'together', 'teacher', 'travel', 'total', 'universe', 'unique', 'upset', 'uncle', 'violence', 'victory', 'victim', 'value', 'weakness', 'winner', 'winter', 'water', 'yogurt', 'youth', 'young', 'zigzag', 'zombie']
+    }
+}
+//function changeDifficulty changes value of variables 'difficulty' and 'wordList when a new difficulty is clicked in settings
